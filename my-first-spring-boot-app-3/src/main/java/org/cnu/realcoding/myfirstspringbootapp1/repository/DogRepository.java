3ae8e2f559de79dcf40094df671b5eb238c502b8
MoonDD99 <mjh991016@naver.com>
2021-04-06 16:31:10 +0900

    initial commit

---
+++ my-first-spring-boot-app-3/src/main/java/org/cnu/realcoding/myfirstspringbootapp1/repository/DogRepository.java
@@ -0,0 +1,34 @@
+package org.cnu.realcoding.myfirstspringbootapp1.repository;
+
+import org.cnu.realcoding.myfirstspringbootapp1.domain.Dog;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class DogRepository {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Dog findDog(String name){
+        return mongoTemplate
+                .findOne(
+                        Query.query(Criteria.where("name").is(name)),
+                        Dog.class
+                );
+    }
+
+    public void insertDog(Dog dog) {
+        mongoTemplate.insert(dog);
+    }
+
+    public List<Dog> findAllDog(){
+        return mongoTemplate.findAll(Dog.class);
+    }
+
+}
